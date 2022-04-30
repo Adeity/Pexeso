216f73e843868b23d5ccf3b06ddf766bec95b1e0
Patrik Pavelka <pavelpa2@fel.cvut.cz>
2022-04-30 11:25:16 +0200

    Adding router and home screen

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
 import {PexesoBoard} from "./PexesoBoard";
-export function myFunction() {
-    const pexesoBoard: PexesoBoard = new PexesoBoard(2, "#pexeso-board")
+
+function initializeNavigation(setRoute: (s: string) => void) {
+    function onHashChanged() {
+        switch (location.hash) {
+            case "#home": setRoute("home"); break;
+            case "#hra": setRoute("hra"); break;
+            default: break;
+        }
+    }
+    window.addEventListener('hashchange', onHashChanged, false);
+}
+
+
+function initializeViewHra() {
+    const pexesoBoard = new PexesoBoard(4, "#pexeso-board")
     pexesoBoard.fillBoard()
 }
-myFunction()
 
+
+class App {
+    constructor() {
+        const defaultRoute = "home"
+        this.setRoute(defaultRoute)
+
+        // Initialize navigation
+        initializeNavigation(this.setRoute);
+    }
+
+    setRoute(route: string) {
+        const views = document.querySelectorAll("[data-view]");
+        views.forEach(function(view) {
+            view.classList.remove("active")
+        })
+        document.querySelector("[data-view="+route+"]").classList.add("active");
+        console.log("Route changed to " + route);
+    }
+}
+
+new App()
